afbe05752709172895a0ee8ef07e02a9b5830ddf
Allen Pereira <allenpereira17@gmail.com>
2023-02-10 10:59:02 -0500

    setup re-frame10x

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
+
 (defproject sample3 "0.1.0-SNAPSHOT"
 
   :description "FIXME: write description"
@@ -11,6 +12,9 @@
                  [com.google.javascript/closure-compiler-unshaded "v20220803"]
                  [cprop "0.1.19"]
                  [day8.re-frame/http-fx "0.2.4"]
+                 [day8.re-frame/tracing      "0.6.2"]
+                 [day8.re-frame/re-frame-10x "1.2.2"]
+                 [day8.re-frame/tracing-stubs "0.5.3"]
                  [expound "0.9.0"]
                  [funcool/struct "1.4.0"]
                  [json-html "0.4.7"]
@@ -41,54 +45,56 @@
                  [thheller/shadow-cljs "2.20.3" :scope "provided"]]
 
   :min-lein-version "2.0.0"
-  
+
   :source-paths ["src/clj" "src/cljs" "src/cljc"]
   :test-paths ["test/clj"]
   :resource-paths ["resources" "target/cljsbuild"]
   :target-path "target/%s/"
   :main ^:skip-aot sample3.core
-
-  :plugins [] 
+  :js-options {:resolve {"highlight.js" {:target :npm :require "highlight.js/lib/core"}}}
+  :plugins []
   :clean-targets ^{:protect false}
   [:target-path "target/cljsbuild"]
-  
+
 
   :profiles
   {:uberjar {:omit-source true
-             
+
              :prep-tasks ["compile" ["run" "-m" "shadow.cljs.devtools.cli" "release" "app"]]
              :aot :all
              :uberjar-name "sample3.jar"
-             :source-paths ["env/prod/clj"  "env/prod/cljs" ]
+             :source-paths ["env/prod/clj"  "env/prod/cljs"]
              :resource-paths ["env/prod/resources"]}
 
    :dev           [:project/dev :profiles/dev]
    :test          [:project/dev :project/test :profiles/test]
 
-   :project/dev  {:jvm-opts ["-Dconf=dev-config.edn" ]
+   :project/dev  {:jvm-opts ["-Dconf=dev-config.edn"]
                   :dependencies [[binaryage/devtools "1.0.6"]
                                  [cider/piggieback "0.5.3"]
                                  [org.clojure/tools.namespace "1.3.0"]
                                  [pjstadig/humane-test-output "0.11.0"]
                                  [prone "2021-04-23"]
-                                 [re-frisk "1.6.0"]
+                                 [day8.re-frame/tracing "0.6.2"]
+                                 [day8.re-frame/re-frame-10x "1.2.2"]
+                                 [day8.re-frame/tracing-stubs "0.5.3"]
                                  [ring/ring-devel "1.9.6"]
                                  [ring/ring-mock "0.4.0"]]
                   :plugins      [[com.jakemccrary/lein-test-refresh "0.24.1"]
                                  [jonase/eastwood "1.2.4"]
-                                 [cider/cider-nrepl "0.26.0"]] 
-                  
-                  
-                  :source-paths ["env/dev/clj"  "env/dev/cljs" "test/cljs" ]
+                                 [cider/cider-nrepl "0.26.0"]]
+
+
+                  :source-paths ["env/dev/clj"  "env/dev/cljs" "test/cljs"]
                   :resource-paths ["env/dev/resources"]
                   :repl-options {:init-ns user
                                  :timeout 120000}
                   :injections [(require 'pjstadig.humane-test-output)
                                (pjstadig.humane-test-output/activate!)]}
-   :project/test {:jvm-opts ["-Dconf=test-config.edn" ]
-                  :resource-paths ["env/test/resources"] 
-                  
-                  
-                  }
+   :project/test {:jvm-opts ["-Dconf=test-config.edn"]
+                  :resource-paths ["env/test/resources"]}
+
+
+
    :profiles/dev {}
    :profiles/test {}})
